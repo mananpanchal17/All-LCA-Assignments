1b93186555fac5c6a83199bdc7ffe08d252cb309
Manan Panchal <175622163+mananpanchal17@users.noreply.github.com>
2026-08-25 09:42:48 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,10 @@
+#Largest Three Numbers
+n1=int(input("Enter First Number: "))
+n2=int(input("Enter Second Number: "))
+n3=int(input("Enter Third Number: "))
+if (n1>n2 and n1>n3):
+  print("The Largest Number is: ", n1)
+if(n2>n1 and n2>n3):
+  print("The Largest Number: ", n2)
+else:
+  print("The Largest Number: 23", n3)
